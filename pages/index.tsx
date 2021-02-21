19adbefe1870df054a2f2d723f72ccfdd23f09a8
leonunesbs <leonunesbs@gmail.com>
2021-02-21 18:38:23 -0300

    sizes

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,18 +22,18 @@ const Home: React.FC = () => {
             <Flex backgroundColor='rgb(18, 163, 58, 0.22)' w={['290px', '360px']} h={['290px', '360px']} borderRadius='full'  zIndex='100' justify='center' align='center'>
               <Image src='pp.jpg' alt='pp' boxSize={['260px', "330px"]} borderRadius='full' zIndex='100' borderColor='brand.500' borderWidth='25px' />
             </Flex>
-            <Flex backgroundColor='brand.300' w={['300px', '400px']} h={['380px', '400px']} mt='-180px' pt='190px' pb='10px' shadow='md' zIndex='50' borderRadius='md' flexDir='column' textAlign='center' align='center' >
+            <Flex backgroundColor='brand.300' w={['320px', '400px']} h={['390px', '400px']} mt='-180px' pt='190px' pb='10px' shadow='md' zIndex='50' borderRadius='md' flexDir='column' textAlign='center' align='center' >
               <Heading color='brand.100' as='h1' fontWeight='ligth'>LEONARDO NUNES</Heading>
               <Text color='brand.500' as='h4'>acadêmico de medicina</Text>
               <Stack flexGrow={1} align='center' isInline spacing={8}>
                 <Link href='https://instagram.com/leonunesbs' isExternal>
-                  <Icon as={FiInstagram} color='brand.100' w={[6, 10]} h={[6, 10]} _hover={{color:'brand.500'}}/>
+                  <Icon as={FiInstagram} color='brand.100' w={[8, 10]} h={[8, 10]} _hover={{color:'brand.500'}}/>
                 </Link>
                 <Link href='https://www.linkedin.com/in/leonardo-n-38001a100/' isExternal>
-                  <Icon as={FaLinkedinIn} color='brand.100' w={[6, 10]} h={[6, 10]} _hover={{color:'brand.500'}}/>
+                  <Icon as={FaLinkedinIn} color='brand.100' w={[8, 10]} h={[8, 10]} _hover={{color:'brand.500'}}/>
                 </Link>
                 <Link href='https://wa.me/5586981312488' isExternal>
-                  <Icon as={FaWhatsapp} color='brand.100' w={[6, 10]} h={[6, 10]} _hover={{color:'brand.500'}}/>
+                  <Icon as={FaWhatsapp} color='brand.100' w={[8, 10]} h={[8, 10]} _hover={{color:'brand.500'}}/>
                 </Link>
               </Stack>
             </Flex>
